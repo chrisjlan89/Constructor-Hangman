60ce76c9352ec2376d99ad760dc7489d20e27d07
Chrisjlan89 <chrisj17@gmail.com>
2018-01-26 18:14:53 -0500

    pretty much no glitches but would like to add some qulaity control improvements if time allows

--- app.js
+++ app.js
@@ -7,43 +7,44 @@ var wordCon = require("./wordConstructor")
 var letterCon = require("./letters.js")
 var correctGuessesAndBlanks = [];
 var incorrectGuesses = [];
+var randomWord;
+var letter;
+var chosenWord;
 
-// var randomWord = words[Math.floor(Math.random() * words.length)];
 
-var spaces = " "
+function newGame(){
+    correctGuessesAndBlanks = [];
+     randomWord = new wordCon(words)
+     letter = new letterCon()
+     chosenWord = randomWord.word
+     chances = 5;
+    console.log("The random word which was generated : " + chosenWord)
+  
+    
+    for(var i=0; i < chosenWord.length; i++){
+        // if(chosenWord.indexOf(spaces)){
+        //     correctGuessesAndBlanks.push("_")
+        // }
+         correctGuessesAndBlanks.push("_")
+     }
+}
 
 
-var randomWord = new wordCon(words)
-
-var letter = new letterCon()
-var chosenWord = randomWord.word
-
-console.log("The random word which was generated : " + chosenWord)
-
-for(var i=0; i < chosenWord.length; i++){
-    // if(chosenWord.indexOf(spaces)){
-    //     correctGuessesAndBlanks.push("_")
-    // }
-     correctGuessesAndBlanks.push("_")
- }
-
- 
+newGame()
 var incorrectGuesses  =  [];
-
+console.log("\n WELCOMEEE!!!! THE THEME IS....Bruce Springsteen Songs") 
 // Load the NPM Package inquirer
 var askLetter = function() {
+   
 
+  
 
+    if(chances > -1){
+      
 
-
-    if(chances > 0){
-        console.log("\n WELCOMEEE!!!! THE THEME IS....Bruce Springsteen Songs")    
-        console.log("\n Your remaining chances don't mess it up...." + chances +"\n")  
-   // console.log(correctGuessesAndBlanks.join(''));
-   // console.log(chosenWord)
-    letter.gameStatus(chosenWord,correctGuessesAndBlanks, chances)    
+    //letter.gameStatus(chosenWord,correctGuessesAndBlanks, chances)    
 //Create a "Prompt" with a series of questions.
-//gameStatus()
+gameStatus()
  inquirer.prompt([
      // Here we create a basic text prompt.
  
@@ -56,18 +57,16 @@ var askLetter = function() {
    
    ])
    .then(function(inquirerResponse) {
-
     var guessedLetter = inquirerResponse.letter
-    letter.gameStatus(chosenWord,correctGuessesAndBlanks, chances)
     var trueFalse = letter.letterInWord(chosenWord, guessedLetter, correctGuessesAndBlanks, incorrectGuesses)
-     //console.log(ok);
-    // console.log(correctGuessesAndBlanks);
     askLetter()
 
-
-    if (trueFalse == false){
-        letter.gameStatus(chosenWord,correctGuessesAndBlanks, chances)
+      if (trueFalse == false){
+      //  letter.gameStatus(chosenWord,correctGuessesAndBlanks, chances)
         chances--
+        console.log("\n Your remaining chances don't mess it up...." + chances +"\n")
+        
+
                  
     };
 
@@ -77,90 +76,47 @@ var askLetter = function() {
 
     }
   
-    
-
 };
-
-
-askLetter()
-
-// function gameStatus(){
-//     if(chosenWord == correctGuessesAndBlanks){
-//         console.log("You've won")
-        
-//     }
-//     else if(chosenWord !== correctGuessesAndBlanks && chances == 0){
-//         console.log("Better luck next time")
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Originally line 57    
-//     if(randomWord.indexOf(guessedLetter)!== -1){
-       
-//       // Need to call a func that handles the replacment of the letter with underscore.....
-      
-//         // incorrectGuesses.push(inquirerResponse.letter)
-      
-//         for(var i = 0; i < randomWord.length; i++){
-// 			if (randomWord[i] === guessedLetter){
-// 				correctGuessesAndBlanks[i] = guessedLetter;
-				
-// 				console.log(correctGuessesAndBlanks)
-				
-	           
-// 			}
-// 		}
-
-//         word.replaceLetter(randomword, guessedLetter, correctGuessesAndBlanks)
-
-
-
-
-
-//       console.log(guessedLetter);
-      
-     
-//        console.log("yooooooooo");
-//       // console.log(incorrectGuesses)
-    
-//      //word.letterInWord(randomWord,guessedLetter)
-//     askLetter();
-//    // gameStatus()
-//     }
-    // for(var j=0; chosenWord.length; j++){
-   // if(inquirerResponse === incorrectGuesses ){
-        // else{
-        //     console.log(correctGuessesAndBlanks)    
-        // incorrectGuesses.push(inquirerResponse.letter)
-        // console.log("Incorrect guesses " + incorrectGuesses.toString())
-        // askLetter();
-        // }
-   // }
+askLetter();
 //}
-   //  word.letterInWord(word.word)
 
 
-   // originally line 104 after the line 57 block
-    // else{
-    //     console.log("Game over")
-    //     chances--;
-    //    // gameStatus()
-    //     // call a reset
-    // }  
+        function gameStatus(){
+     if(chosenWord == correctGuessesAndBlanks.join('')){
+         console.log("You've won\n")
+         console.log("---------- BEGIN NEW GAME ---------- \n ")
+         newGame();
+        
+        
+     }
+     else if(chosenWord != correctGuessesAndBlanks && chances == 0){
+         console.log("Better luck next time\n")
+         console.log("---------- BEGIN NEW GAME ---------- \n ")
+         newGame();
+        
+     }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- letters.js
+++ letters.js
@@ -1,18 +1,18 @@
 
 
      function Letter(letter){
-        this.gameStatus = function(word, blanks, chances){
-            //console.log(blanks.toString())
-            if(word.toString() === blanks.join('')){
-                console.log("You've won")
+        // this.gameStatus = function(word, blanks, chances,func){
+        //     if(word == blanks.join('')){
+        //         console.log("You've won")
                 
                 
-            }
-            else if(word !== blanks && chances == 0){
-                console.log("Better luck next time")
                 
-            }
-        }
+        //     }
+        //     else if(word != blanks && chances == 0){
+        //         console.log("Better luck next time")
+                
+        //     }
+        // }
         
         this.letterInWord = function(word, letter, blanks, wrongs){
             if(word.indexOf(letter) !== -1){
@@ -22,12 +22,13 @@
                     if(word[i] == letter){
                        
                         // replace a blank on a cmd prompt
-                       //  console.log("Good guess")
+                       
                        blanks[i] = letter;
-                       console.log("\n -----------------------------")
-                       console.log("Here are your incorrect guesses : \n " + wrongs)
-                      // blanks.join('')
-                      // console.log("")
+                       //console.log("\n -----------------------------")
+                      // console.log("Good guess")
+                      // console.log("Here are your incorrect guesses : \n " + wrongs)
+                      
+                      
                     }
                    
                 }
@@ -43,7 +44,7 @@
             wrongs.push(letter)
             console.log("That guess was erroneous on all counts\n")
             console.log("Here are your incorrect guesses : \n " + wrongs)
-            console.log("\n -----------------------------")
+            //console.log("\n -----------------------------")
             return false;
            }
            // console.log(chances)
@@ -51,7 +52,7 @@
            console.log("\nSorry that wrong, and you already guessed that!!!!")
 
            console.log("\nHere are your incorrect guesses in case you forgot...  " + wrongs)
-           console.log("\n -----------------------------\n")
+           console.log("\n")
            
           // console.log(wrongs)
            
